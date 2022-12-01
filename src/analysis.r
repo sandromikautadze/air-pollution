4d2936076a7cef43c4dc4007480d49357b034eb6
Sandro Mikautadze <sandromikautadze@yahoo.com>
2022-12-01 23:24:22 +0100

    Initial commit of src (analysis.r, cleaning.r) analysis.r has some hints to the research questions

---
+++ src/analysis.r
@@ -0,0 +1,10 @@
+# STATISTICAL ANALYSIS
+
+# Hints to research questions
+# Are there statistically significant differences between Northern, Central and
+# Southern Italy in terms of air pollution? (HP Test)
+# Is there enough statistical evidence to prove that Italy's pollution levels
+# exceed the EU guidelines? (HP Test)
+# How are pollutants related to pollution levels? (ANOVA)
+# How are different zones (background, industrial and traffic) related to pollution levels? (ANOVA)
+# Longitude and Latitude vs Air Pollution Level (Regression)
