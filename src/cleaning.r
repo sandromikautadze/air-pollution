47433e941e25f0c1bc4d940ade85b7a631343096
Sandro Mikautadze <sandromikautadze@yahoo.com>
2022-12-02 23:35:13 +0100

    Imported the dataset There's one tibble for each pollutant

--- src/cleaning.r
+++ src/cleaning.r
@@ -1,1 +1,16 @@
 # DATA CLEANING
+
+#libraries
+library(readxl)
+library(tidyverse)
+
+# importing data
+pm10 <- read_excel("data/2018 data -EU values.xlsx", sheet = "PM10", skip = 6)
+pm25 <- read_excel("data/2018 data -EU values.xlsx", sheet = "PM2.5", skip = 5)
+o3 <- read_excel("data/2018 data -EU values.xlsx", sheet = "O3", skip = 7)
+no2 <- read_excel("data/2018 data -EU values.xlsx", sheet = "NO2", skip = 5)
+bap <- read_excel("data/2018 data -EU values.xlsx", sheet = "BaP", skip = 5)
+so2 <- read_excel("data/2018 data -EU values.xlsx", sheet = "SO2", skip = 5)
+
+# exploratory analysis of the data
+
